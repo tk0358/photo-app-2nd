237df03adf781857b7b40459db5a9ed1c8a2f31a
tk0358 <tk0358a@yahoo.co.jp>
2020-03-06 18:00:31 +0900

    add fog_provider to carrier_wave.rb

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,5 +1,6 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider:              'AWS',                        # required
       aws_access_key_id: Rails.application.credentials.aws[:access_key_id],                       # required unless using use_iam_profile
@@ -7,5 +8,6 @@ if Rails.env.production?
       region: 'ap-northeast-1'                        # required 
     }
     config.fog_directory = Rails.application.credentials.aws[:s3_bucket]
+    config.fog_public = false
   end
 end
